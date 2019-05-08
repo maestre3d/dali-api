c6d11a557944f2b8433ebb5d3902133721639bdc
maestre3d <luis.alonso.16@hotmail.com>
2019-05-08 13:00:22 -0600

    Updated from bcrypt to bcryptjs lib.

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 'use strict'
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('../services/jwt');
 const fs = require('fs');
 const path = require('path');
