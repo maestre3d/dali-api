77fd5f26cf868f9d5cae92a265feb63fdf9ff9f0
maestre3d <luis.alonso.16@hotmail.com>
2019-05-25 03:43:48 -0600

    Clustering server module

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-let mongoose = require('mongoose');
-let app = require('./app');
+const cluster = require('cluster');
+const mongoose = require('mongoose');
+const app = require('./app');
 const PORT = process.env.PORT || 3977;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/dali', {useNewUrlParser: true}, (err, res)=>{
-    if(err){
+if (cluster.isMaster) {
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-    }else{
-        console.log('Connected to DB Server');
-        app.listen(PORT, function(){
-            console.log('API Listening on ' + PORT);
-        });
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
     }
-});
+
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker,
+        // we're not sentimental
+        console.log('Worker %d died :(', worker.id);
+        cluster.fork();
+    });
+} else {
+    mongoose.connect('mongodb://localhost:27017/dali', {useNewUrlParser: true}, (err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log('Connected to DB Server');
+            app.listen(PORT, function(){
+                console.log('API Listening on ' + PORT);
+            });
+        }
+    });
+}
